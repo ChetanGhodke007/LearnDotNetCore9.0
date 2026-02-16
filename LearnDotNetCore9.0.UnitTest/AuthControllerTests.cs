9e6b6a61e357f45d1308a999c535dd552e14c9b8
Chetan Ghodke <cghodke@merchante.com>
2026-02-16 22:21:47 +0530

    unit test case

---
+++ LearnDotNetCore9.0.UnitTest/AuthControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using LearnDotNetCore9._0.Controllers;
+using LearnDotNetCore9._0.Domain;
+using LearnDotNetCore9._0.DTO;
+
+public class AuthControllerTests
+{
+    private readonly AuthController _controller;
+
+    public AuthControllerTests()
+    {
+        // Initialize the controller
+        _controller = new AuthController();
+    }
+
+    [Fact]
+    public void Login_ReturnsOk_WhenCredentialsAreValid()
+    {
+        // Arrange
+        var validUser = new LoginModel { UserName = "a", Password = "b" };
+
+        // Act
+        var result = _controller.Login(validUser);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var response = Assert.IsType<AuthenticatedResponse>(okResult.Value);
+        Assert.NotNull(response.Token);
+    }
+
+    [Fact]
+    public void Login_ReturnsUnauthorized_WhenCredentialsAreInvalid()
+    {
+        // Arrange
+        var invalidUser = new LoginModel { UserName = "wrong", Password = "wrong" };
+
+        // Act
+        var result = _controller.Login(invalidUser);
+
+        // Assert
+        Assert.IsType<UnauthorizedResult>(result);
+    }
+
+    [Fact]
+    public void Login_ReturnsBadRequest_WhenUserIsNull()
+    {
+        // Act
+        var result = _controller.Login(null);
+
+        // Assert
+        var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+        Assert.Equal("Invalid client request", badRequestResult.Value);
+    }
+}
